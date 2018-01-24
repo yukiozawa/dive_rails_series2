86e67c86a4770e80ba5453b451dfd3ed65b3dc1b
yuki <yukimomo001@gmail.com>
2018-01-24 18:34:03 +0900

    heroku でsendgridを使う設定

--- app/mailers/blog_post_mailer.rb
+++ app/mailers/blog_post_mailer.rb
@@ -2,6 +2,7 @@ class BlogPostMailer < ApplicationMailer
   def blog_post(blog)
     @blog = blog
     @user_email = @blog.user.email
+    # binding.pry
     mail to: @user_email, subject: "ブログ内容の確認メール"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,9 @@ Rails.application.routes.draw do
 
   resources :favorites, only: [:create, :destroy, :index]
 
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
